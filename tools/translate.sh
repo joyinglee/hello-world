033119e1ae582573d950d2beb1c5f5f827e41233
鑫乐渡渡 <43372606+joyinglee@users.noreply.github.com>
2020-12-07 21:48:20 +0800

    翻译脚本
    翻译并同步到欧路词典的单词本。

---
+++ tools/translate.sh
@@ -0,0 +1,8 @@
+word=`xclip -out`
+#echo "$word" >> his.txt
+echo -e `sdcv -n -c "$word"`
+read -p "同步到手机?" oulu
+if [ "$oulu" = "y" ]; then
+curl -H "Authorization:认证码"  -d "id=0&language=en&words=$word" https://api.frdic.com/api/open/v1/studylist/words
+fi
+echo
