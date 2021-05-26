5704817aec70796eb0fd8ceb463f3cff56e19e2c
鑫乐渡渡 <43372606+joyinglee@users.noreply.github.com>
2021-05-26 22:33:22 +0800

    更新POST的姿势
    可能是服务器对POST的格式要求变严格了，原格式已无效。

--- tools/translate.sh
+++ tools/translate.sh
@@ -3,6 +3,6 @@ word=`xclip -out`
 echo -e `sdcv -n -c "$word"`
 read -p "同步到手机?" oulu
 if [ "$oulu" = "y" ]; then
-curl -H "Authorization:认证码"  -d "id=0&language=en&words=$word" https://api.frdic.com/api/open/v1/studylist/words
+curl -H 'Content-Type: application/json' -H 'Authorization: 认证码' -d '{"id":"0","language":"en","words":["'$word'"]}' https://api.frdic.com/api/open/v1/studylist/words
 fi
 echo
